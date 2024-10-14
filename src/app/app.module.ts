3927d9219f08c6ba5bf13bd55287f9203d59c389
IGNACIOb21 <ig.aguilaruocuc.cl>
2024-10-13 21:37:46 -0300

    Tu mensaje de commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"app-futbol-base","appId":"1:210395209272:web:0c502b770c3ea70b5510cd","storageBucket":"app-futbol-base.appspot.com","apiKey":"AIzaSyCJ3YqMfQBp_QG7ul8e0d2PH3UB3UKIQJQ","authDomain":"app-futbol-base.firebaseapp.com","messagingSenderId":"210395209272","measurementId":"G-KQ12FM5WE4"})), provideAuth(() => getAuth())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
