752392d30173cdc8ff266a057c40920d5d8c99f2
IGNACIOb21 <ig.aguilaruocuc.cl>
2024-11-04 22:15:06 -0300

    base de datos conexion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCJ3YqMfQBp_QG7ul8e0d2PH3UB3UKIQJQ",
-    authDomain: "app-futbol-base.firebaseapp.com",
-    projectId: "app-futbol-base",
-    storageBucket: "app-futbol-base.appspot.com",
-    messagingSenderId: "210395209272",
-    appId: "1:210395209272:web:0c502b770c3ea70b5510cd",
-    measurementId: "G-KQ12FM5WE4"
+    apiKey: "AIzaSyCwT2__mSbqUpJgGVX9EP2JTgU8QU4lUnI",
+  authDomain: "proyecto-qr-8578b.firebaseapp.com",
+  projectId: "proyecto-qr-8578b",
+  storageBucket: "proyecto-qr-8578b.firebasestorage.app",
+  messagingSenderId: "610904168297",
+  appId: "1:610904168297:web:4ff44cbb716cd164fe8ff8",
+  measurementId: "G-GES9S0KW2T"
   }
 };
 
