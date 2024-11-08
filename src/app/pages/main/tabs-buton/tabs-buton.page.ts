a53e01cde1b97689632bf29a491cb2141375a692
IGNACIOb21 <166768975+IGNACIOb21@users.noreply.github.com>
2024-11-08 15:13:39 -0300

    MEGRA DEL CODIGO

--- src/app/pages/main/tabs-buton/tabs-buton.page.ts
+++ src/app/pages/main/tabs-buton/tabs-buton.page.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabs-buton',
   templateUrl: './tabs-buton.page.html',
   styleUrls: ['./tabs-buton.page.scss'],
 })
-export class TabsButonPage implements OnInit {
+export class TabsButonPage  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  
 
 }
