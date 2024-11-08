f162d28d90f6a92483cbcb27197b0a014b0ed3d1
IGNACIOb21 <166768975+IGNACIOb21@users.noreply.github.com>
2024-11-08 15:31:13 -0300

    AAAA

--- src/app/pages/main/escaner-qr/escaner-qr.module.ts
+++ src/app/pages/main/escaner-qr/escaner-qr.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { EscanerQRPageRoutingModule } from './escaner-qr-routing.module';
 
 import { EscanerQRPage } from './escaner-qr.page';
+import { SharedModule } from "../../../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    EscanerQRPageRoutingModule
+    EscanerQRPageRoutingModule,
+    SharedModule
   ],
   declarations: [EscanerQRPage]
 })
