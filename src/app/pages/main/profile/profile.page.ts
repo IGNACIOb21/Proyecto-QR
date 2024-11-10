8574cd554669ee0f753a361c88cdf2d80692c1dd
Reaktion-cmd <137962840+Reaktion-cmd@users.noreply.github.com>
2024-11-10 20:45:56 -0300

    perfil alumno sin nada funcional
    Creo que ahi estaria el perfil , si es posible. revisar y entregar feedback , intente revisar si funcionaba pero "ionic serve" me daba error por algun motico

--- src/app/pages/main/profile/profile.page.ts
+++ src/app/pages/main/profile/profile.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.page.html',
-  styleUrls: ['./profile.page.scss'],
+  selector: 'app-student-profile',
+  templateUrl: './student-profile.page.html',
+  styleUrls: ['./student-profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class StudentProfilePage {
+  student = {
+    photoUrl:'https://photos.app.goo.gl/GyfCLgCntPtBmZiA8', // URL de la foto del alumno
+    name: 'Juan Pérez',
+    email: 'juan.perez@example.com',
+    career: 'Ingeniería en Informática'
+  };
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
