fb7d3214d052b384ed62fe93dfbe3f0582bf2a83
IGNACIOb21 <ig.aguilaruocuc.cl>
2024-12-13 20:53:23 -0300

    funcional

--- src/app/pages/main/escaner-qr/escaner-qr.page.ts
+++ src/app/pages/main/escaner-qr/escaner-qr.page.ts
@@ -3,6 +3,7 @@ import { BarcodeScanner, LensFacing } from '@capacitor-mlkit/barcode-scanning';
 import { ModalController, Platform } from '@ionic/angular';
 import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-escaner-qr',
@@ -16,7 +17,8 @@ export class EscanerQRPage implements OnInit {
   constructor(
     private modalController: ModalController,
     private platform: Platform,
-    private firebaseService: FirebaseService // Inyectamos FirebaseService
+    private firebaseService: FirebaseService, // Inyectamos FirebaseService
+    private toastController: ToastController // Inyectamos ToastController
   ) {}
 
   ngOnInit(): void {
@@ -25,6 +27,15 @@ export class EscanerQRPage implements OnInit {
       BarcodeScanner.checkPermissions().then();
       BarcodeScanner.removeAllListeners();
     }
+  }
+
+  async presentToast(message: string, color: string = 'danger') {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000,
+      color,
+    });
+    await toast.present();
   }
 
   async startScan() {
@@ -41,6 +52,13 @@ export class EscanerQRPage implements OnInit {
     if (data) {
       this.scanResult = data?.barcode.displayValue; // Guardamos el resultado del escaneo
 
+      // Validar el formato del código QR
+      const qrCodePattern = /^[A-Z]{3}[0-9]{3}\/\d{3}[A-Z]\/L\d+$/; // Patrón esperado
+      if (!qrCodePattern.test(this.scanResult)) {
+        await this.presentToast('El código escaneado no es válido en esta operación.', 'danger');
+        return; // Terminar el flujo si el código no es válido
+      }
+
       // Obtener el UID del usuario autenticado
       const user = await this.firebaseService.getUserData();
       const uid = user?.uid;
@@ -49,9 +67,9 @@ export class EscanerQRPage implements OnInit {
       if (uid) {
         const success = await this.firebaseService.processScanResult(this.scanResult, uid);
         if (success) {
-          console.log('Asistencia incrementada correctamente.');
+          await this.presentToast('El código QR se escaneó exitosamente.', 'success');
         } else {
-          console.error('No se encontró una asignatura válida para el escaneo.');
+          await this.presentToast('No se encontró una asignatura válida para el escaneo.', 'danger');
         }
       }
     }
